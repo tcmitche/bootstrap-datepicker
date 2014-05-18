5955ebf748bfac004deec590aa812602991366bb
Travis Mitchell <contact@traviscmitchell.com>
2014-05-18 10:24:56 -0700

    Attached to input, year and month display.

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-function addBox(x, y) {
-    var box = document.createElement('div');
+(function() {
+var INPUT;
+var YEAR = new Date().getFullYear();
+var MONTH = new Date().getMonth();
+var DAY = new Date().getDate();
+var BOX;
+
+function div() { return document.createElement('div');}
+function button(value) {
+    var button = document.createElement('button');
+    button.className = 'btn btn-default';
+    button.value = value;
+    button.textContent = value;
+    return button;
+}
+function wrappedButton(value) {
+    var wrap = div();
+    wrap.className = 'btn-group';
+    wrap.appendChild(button(value));
+    return wrap;
+}
+
+function createContainer(x, y) {
+    var box = div();
     var s = box.style;
     function px(n) { return n + 'px'; }
     s.position = 'absolute';
     s.left = px(x);
     s.top = px(y);
+    s.width = px(322);
     s.backgroundColor = '#e8e8e8';
     return box;
 }
@@ -19,20 +42,27 @@ function daysInMonth(year, month) {
     return new Date(year, month + 1, 0).getDate();
 }
 
+function outputDate(event) {
+    var DAY = event.target.value;
+    var date = new Date(YEAR, MONTH, DAY);
+    INPUT.value = date.toLocaleDateString();
+    destroyCalendar();
+}
+
 function dateButton(date, disabled) {
-    var button = document.createElement('button');
-    button.value = date;
-    button.className = 'btn btn-default calendar-date';
-    button.textContent = date;
+    var b = button(date);
+    b.className = 'btn btn-default calendar-date';
     if (disabled) {
-        button.disabled = 'disabled';
+        b.disabled = 'disabled';
+    } else {
+        b.onclick = outputDate;
     }
-    return button;
+    return b;
 }
 
 function renderDateButtons(year, month, disabled) {
     var days = daysInMonth(year, month);
-    //var week = document.createElement('div');
+    //var week = div();
     var i;
     var startingDay = new Date(year, month, 1).getDay();
     var buttons = [];
@@ -55,17 +85,70 @@ function renderDateButtons(year, month, disabled) {
     return buttons;
 }
 
-function showCalendar(elem) {
-    var pos = elem.getBoundingClientRect();
-    var box = addBox(pos.left, pos.botttom);
-    var b = renderDateButtons(new Date().getFullYear(), new Date().getMonth() - 2);
-    var week = document.createElement('div');
-    for (var i = 1; i <= b.length; i++) {
-        week.appendChild(b[i - 1]);
-        if (i % 7 === 0 || i === b.length) {
-            box.appendChild(week);
-            week = document.createElement('div');
+function previousMonth() {
+    if (--MONTH < 0) {
+        YEAR--;
+        MONTH = 11;
+    }
+    renderCalendar();
+}
+
+function nextMonth() {
+    if (++MONTH > 11) {
+        YEAR++;
+        MONTH = 0;
+    }
+    renderCalendar();
+}
+
+function createControls() {
+    var container = div();
+    container.className = 'btn-group btn-group-justified controls';
+    var prev = wrappedButton('<');
+    var year = wrappedButton(YEAR);
+    var month = wrappedButton(MONTH);
+    var next = wrappedButton('>');
+    prev.onclick = previousMonth;
+    next.onclick = nextMonth;
+    container.appendChild(prev);
+    container.appendChild(year);
+    container.appendChild(month);
+    container.appendChild(next);
+    return container;
+}
+
+function renderCalendar() {
+    BOX.innerHTML = '';
+    var controls = createControls();
+    var buttons = renderDateButtons(YEAR, MONTH);
+    var week = div();
+    BOX.appendChild(controls);
+    for (var i = 1; i <= buttons.length; i++) {
+        week.appendChild(buttons[i - 1]);
+        if (i % 7 === 0 || i === buttons.length) {
+            week.className = 'week-' + (i / 7);
+            BOX.appendChild(week);
+            week = div();
         }
     }
-    document.body.appendChild(box);
 }
+
+function createCalendar(elem) {
+    INPUT = elem;
+    var pos = elem.getBoundingClientRect();
+    BOX = createContainer(pos.left, pos.botttom);
+    renderCalendar();
+    document.body.appendChild(BOX);
+}
+
+function destroyCalendar() {
+    document.body.removeChild(BOX);
+    BOX = undefined;
+}
+
+function DatePicker(elem) {
+    if (BOX) destroyCalendar();
+    else     createCalendar(elem);
+}
+window.DatePicker = DatePicker;
+}());
